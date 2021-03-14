fd03fdc01257d59a086d0e637c7e1b50602898a9
fata1-err0r <29376868+fata1-err0r@users.noreply.github.com>
2021-03-14 12:43:57 +0700

    fixes

---
+++ HW2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bb wrld!");
+            
+        }
+    }
+}
