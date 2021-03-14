ac28440de231c2060d01d8b703fe78cce5a0cc36
fata1-err0r <29376868+fata1-err0r@users.noreply.github.com>
2021-03-14 15:38:57 +0700

    HomeWork is complete

--- HW2.1/Program.cs
+++ HW2.1/Program.cs
@@ -2,12 +2,57 @@
 
 namespace HW2._1
 {
-    class Program1
-    {
-        static void Main(string[] args)
-        {
-			Console.WriteLine("Choose a months (1-12): ");
-            
-        }
-    }
+	class Program1
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Enter the ordinal number of the month (1-12)");
+				int month = Convert.ToInt32(Console.ReadLine());
+				switch (month)
+				{
+					case 1:
+						Console.WriteLine("January");
+						break;
+					case 2:
+						Console.WriteLine("February");
+						break;
+					case 3:
+						Console.WriteLine("March");
+						break;
+					case 4:
+						Console.WriteLine("April");
+						break;
+					case 5:
+						Console.WriteLine("May");
+						break;
+					case 6:
+						Console.WriteLine("June");
+						break;
+					case 7:
+						Console.WriteLine("July");
+						break;
+					case 8:
+						Console.WriteLine("August");
+						break;
+					case 9:
+						Console.WriteLine("September");
+						break;
+					case 10:
+						Console.WriteLine("October");
+						break;
+					case 11:
+						Console.WriteLine("November");
+						break;
+					case 12:
+						Console.WriteLine("December");
+						break;
+					default:
+						Console.WriteLine("ERROR");
+						break;
+				}
+			}
+		}
+	}
 }
