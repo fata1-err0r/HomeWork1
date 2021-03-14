cc03371b308c7ff260c54e6f0af208aa8f4bed4b
fata1-err0r <29376868+fata1-err0r@users.noreply.github.com>
2021-03-14 22:09:51 +0700

    minor fixes

--- HW2.1/Program.cs
+++ HW2.1/Program.cs
@@ -9,43 +9,43 @@ namespace HW2._1
 			while (true)
 			{
 				Console.WriteLine("Enter the ordinal number of the month (1-12) ");
-				int month = Convert.ToInt32(Console.ReadLine());
+				string month = (Console.ReadLine());
 				switch (month)
 				{
-					case 1:
+					case "1":
 						Console.WriteLine("January");
 						break;
-					case 2:
+					case "2":
 						Console.WriteLine("February");
 						break;
-					case 3:
+					case "3":
 						Console.WriteLine("March");
 						break;
-					case 4:
+					case "4":
 						Console.WriteLine("April");
 						break;
-					case 5:
+					case "5":
 						Console.WriteLine("May");
 						break;
-					case 6:
+					case "6":
 						Console.WriteLine("June");
 						break;
-					case 7:
+					case "7":
 						Console.WriteLine("July");
 						break;
-					case 8:
+					case "8":
 						Console.WriteLine("August");
 						break;
-					case 9:
+					case "9":
 						Console.WriteLine("September");
 						break;
-					case 10:
+					case "10":
 						Console.WriteLine("October");
 						break;
-					case 11:
+					case "11":
 						Console.WriteLine("November");
 						break;
-					case 12:
+					case "12":
 						Console.WriteLine("December");
 						break;
 					default:
